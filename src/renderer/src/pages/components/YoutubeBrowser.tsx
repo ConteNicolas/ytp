7efc65a86999a54615f3974e2971664e5755bfbc
Nicolas Conte <contenicolascv@gmail.com>
2024-12-13 19:45:58 -0300

    nodeintegration and browser component

--- src/renderer/src/pages/components/YoutubeBrowser.tsx
+++ src/renderer/src/pages/components/YoutubeBrowser.tsx
@@ -4,12 +4,24 @@ import { Search } from "lucide-react";
 import React from "react";
 
 
+const yts = require('yt-search');
 
 const YoutubeBrowser = () => {
+
+    const [search, setSearch] = React.useState("");
+
+    const handleSearch = async () => {
+        if (!search || !search.length) return;
+
+        await window.api.ytSearch(search).then((data: any) => {
+            console.log(data);
+        });
+    }
+
     return (
         <React.Fragment>
-            <Input className="w-[75%] rounded-l-full border-slate-800 focus:border-slate-800" placeholder="Buscar video" />
-            <Button className="w-[60px] rounded-r-full"><Search /></Button>
+            <Input onChange={(e: any) => setSearch(e.target.value)} className="w-[75%] rounded-l-full border-slate-800 focus:border-slate-800" placeholder="Buscar video" />
+            <Button className="w-[60px] rounded-r-full" onClick={handleSearch} ><Search /></Button>
         </React.Fragment>
     )
 }
