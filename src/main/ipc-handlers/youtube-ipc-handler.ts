c7e881f5167dc553a3ebbdb62a4cc2bc4a93d012
Nicolas Conte <contenicolascv@gmail.com>
2024-12-16 19:57:56 -0300

    remove unused param

--- src/main/ipc-handlers/youtube-ipc-handler.ts
+++ src/main/ipc-handlers/youtube-ipc-handler.ts
@@ -9,8 +9,8 @@ const youtubeService = new YoutubeService();
 
 export const registerYoutubeIpcHandler = () => {
   ipcMain.handle('download:video', async (event, item) => {  
-    return await youtubeService.enqueueDownload(item, (success, message, status) => {
-      event.sender.send('download:video:response', { success, message, status });
+    return await youtubeService.enqueueDownload(item, (success, message) => {
+      event.sender.send('download:video:response', { success, message });
     });
   });
 }
