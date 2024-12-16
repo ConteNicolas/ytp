cbeedb569d24480f975d33f924d6925fa7cfbae0
Nicolas Conte <contenicolascv@gmail.com>
2024-12-16 10:11:03 -0300

    fix: improve performance by queuing the songs

--- src/main/ipc-handlers/youtube-ipc-handler.ts
+++ src/main/ipc-handlers/youtube-ipc-handler.ts
@@ -9,8 +9,8 @@ const youtubeService = new YoutubeService();
 
 export const registerYoutubeIpcHandler = () => {
   ipcMain.handle('download:video', async (event, item) => {  
-    return await youtubeService.downloadVideoAsMp3(item, (success, message) => {
-      event.sender.send('download:video:response', { success, message });
+    return await youtubeService.enqueueDownload(item, (success, message, status) => {
+      event.sender.send('download:video:response', { success, message, status });
     });
   });
 }
--- src/main/services/youtube-service.ts
+++ src/main/services/youtube-service.ts
@@ -1,100 +1,144 @@
 import { spawn } from "child_process";
 import { app } from "electron";
-
 import fs from "fs";
 import path from "path";
 import { SettingService } from "./setting-service";
 import { HistoryService } from "./history-service";
+import { v4 as uuidv4 } from "uuid";
 
-import { v4 as uuidv4 } from 'uuid';
+interface HistoryItem {
+  id: string;
+  url: string;
+  title: string;
+  thumbnail: string;
+  status: "in-queue" | "downloading" | "downloaded" | "error";  
+  reason: string | null;
+  time: string;
+  outputDir: string;
+}
 
 export class YoutubeService {
   private _settingService = new SettingService();
   private _historyService = new HistoryService();
+  private isProcessing = false;
 
-  public async downloadVideoAsMp3(
+  private _isQueueCompleted = false;
+
+  constructor() {
+    this.resumePendingDownloads();
+  }
+
+  public enqueueDownload(
     item: any,
     onDownloadFinishCallback: (success: boolean, message: string) => void
   ) {
-    // Verificar si la canción ya fue descargada
     if (this._historyService.alreadyDownloaded(item.url, item.title)) {
-      onDownloadFinishCallback(true, `Canción ${item.title} ya ha sido descargada.`);
+      onDownloadFinishCallback(true, `Cancion "${item.title}" ya fue descargada.`);
       return;
     }
 
-    console.log("Descargando ", item.url);
-
-    const ytDlpPath = path.join(app.getAppPath(), '/resources/yt-dlp.exe');
-
-    if (!fs.existsSync(ytDlpPath)) {
-      onDownloadFinishCallback(false, 'yt-dlp.exe not found.');
-      return;
-    }
-
-    const output = this._settingService.getSettingValue('pendrive_path');
-    console.log("Carpeta de salida ", output);
-
-    // Configuración de los argumentos para la descarga
-    const args = ['-x', '--audio-format', 'mp3', '-o', `${output}/%(title)s.%(ext)s`, item.url];
-
-    // Generar un ID único para esta descarga
     const id = uuidv4();
+    const newItem: HistoryItem = {
+      id,
+      url: item.url,
+      title: item.title,
+      thumbnail: item.thumbnail,
+      status: "in-queue",
+      reason: null,
+      time: new Date().toISOString(),
+      outputDir: this._settingService.getSettingValue("pendrive_path"),
+    };
 
-    // Iniciar el proceso de descarga
-    const process = spawn(ytDlpPath, args);
+    this._historyService.addToHistory(newItem);
+    console.log(`Cancion en cola: ${item.title}`);
+    onDownloadFinishCallback(false, `Cancion "${item.title}" en cola.`);
 
-    console.log("Descarga en progreso...");
-
-    // Notificar que la descarga ha iniciado
-    onDownloadFinishCallback(false, `Comenzando descarga de ${item.title}...`);
-
-    // Escuchar cuando el proceso se cierra
-    process.on('close', (code) => {
-      console.log(`Proceso finalizado con código: ${code}`);
-
-      if (code !== 0) {
-        console.error("Error durante la descarga");
-        this._historyService.addToHistory({
-          id: id,
-          url: item.url,
-          title: item.title,
-          thumbnail: item.thumbnail,
-          isSuccess: false,
-          reason: `Error: Código de salida ${code}`,
-          time: new Date(),
-          outputDir: output
-        });
-        onDownloadFinishCallback(false, `Error: No se pudo descargar ${item.title}.`);
-      } else {
-        console.log("Descarga completa");
-        this._historyService.addToHistory({
-          id: id,
-          url: item.url,
-          title: item.title,
-          thumbnail: item.thumbnail,
-          isSuccess: true,
-          reason: "Descarga completa",
-          time: new Date(),
-          outputDir: output
-        });
-        onDownloadFinishCallback(true, `Canción ${item.title} descargada exitosamente.`);
-      }
-    });
-
-    // Escuchar errores del proceso
-    process.on('error', (err) => {
-      console.error(`Error al ejecutar el proceso: ${err.message}`);
-      this._historyService.addToHistory({
-        id: id,
-        url: item.url,
-        title: item.title,
-        thumbnail: item.thumbnail,
-        isSuccess: false,
-        reason: `Error al ejecutar el proceso: ${err.message}`,
-        time: new Date(),
-        outputDir: output
+    if (!this.isProcessing && this._isQueueCompleted) {
+      this.processQueue(() => {
+        onDownloadFinishCallback(true, "Todas las descargas han finalizado.");
       });
-      onDownloadFinishCallback(false, `Error: ${err.message}`);
+    }
+  }
+
+  private async processQueue(onAllDownloadsComplete: () => void) {
+    this.isProcessing = true;
+    this._isQueueCompleted = false;
+  
+    const downloads = this.getDownloads();
+  
+    for (const item of downloads) {
+      if (item.status === "in-queue" || item.status === "downloading") {
+        item.status = "downloading";
+        this._historyService.updateHistory(item);
+  
+        const success = await this.downloadSong(item);
+  
+        if (success) {
+          item.status = "downloaded";
+          item.reason = "Descarga completada.";
+        } else {
+          item.status = "error";
+          item.reason = `Error: Falló la descarga.`;
+        }
+  
+        item.time = new Date().toISOString();
+        this._historyService.updateHistory(item);
+      }
+    }
+  
+    const downloadsUntilNow = this.getDownloads();
+  
+    if (downloadsUntilNow.some((x) => x.status === "in-queue")) {
+      console.log("Hay descargas pendientes, reanudando...");
+      await this.processQueue(onAllDownloadsComplete);
+      return;
+    }
+  
+    if (!this._isQueueCompleted) {
+      this._isQueueCompleted = true;
+      this.isProcessing = false;
+  
+      console.log("Todas las descargas han finalizado.");
+      console.log(
+        "Cantidad total de canciones descargadas: ",
+        this.getDownloads().filter((x) => x.status === "downloaded").length
+      );
+  
+      onAllDownloadsComplete();
+    }
+  }
+
+
+  private downloadSong(item: HistoryItem): Promise<boolean> {
+    return new Promise((resolve) => {
+      const ytDlpPath = path.join(app.getAppPath(), "/resources/yt-dlp.exe");
+
+      if (!fs.existsSync(ytDlpPath)) {
+        console.error("yt-dlp.exe no encontrado.");
+        resolve(false);
+        return;
+      }
+
+      const args = ["-x", "--audio-format", "mp3", "-o", `${item.outputDir}/%(title)s.%(ext)s`, item.url];
+      const process = spawn(ytDlpPath, args);
+
+      process.on("close", (_) => resolve(true));
+
+      process.on("error", (err) => {
+        console.error(`Error: ${err.message}`);
+        resolve(false);
+      });
+    });
+  }
+
+  private getDownloads(): HistoryItem[] {
+    return this._historyService.getHistory();
+  }
+
+  private resumePendingDownloads() {
+    console.log("Reanudando descargas pendientes...");
+    this.processQueue(() => {
+      console.log("Todas las descargas pendientes han sido procesadas.");
     });
   }
 }
