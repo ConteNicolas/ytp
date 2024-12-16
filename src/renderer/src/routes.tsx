249eeac218401c20463dfd3547dabd90c2212aa8
Nicolas Conte <contenicolascv@gmail.com>
2024-12-16 13:10:35 -0300

    feat: add setting route

--- src/renderer/src/routes.tsx
+++ src/renderer/src/routes.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home/Home";
+import Setting from "./pages/Setting/Setting";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Home />,
+    },
+    {
+        path: "/setting",
+        element: <Setting />,
     }
 ])
 
